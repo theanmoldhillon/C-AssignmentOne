9c27130eed9248a65391914a94ee8ba84f0beac9
Anmol-Singh <anmoldhillon999@gmail.com>
2018-09-21 09:41:04 -0500

    First Commit

---
+++ AssignmentOne/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentOne
+{
+    public class House
+    {
+        public int Value { get; set; }
+        public int LotSize { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ AssignmentOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssignmentOne.Startup))]
+namespace AssignmentOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
